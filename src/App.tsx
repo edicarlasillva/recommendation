10299c86a2e08cc1522ff1a6f78db6efc8082c9b
Carla Silva <carla.silva@movestock.com>
2024-03-25 20:55:29 -0300

    chore: config redux persist

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { Provider as ReduxProvider } from "react-redux";
-import { RecommendationPage } from "./components/RecommendationPage";
 
-import { store } from "./store/store";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/Router";
+
+import { PersistGate } from "redux-persist/integration/react";
+import { persitedStore, store } from "./store/store";
 
 export function App() {
   return (
     <ReduxProvider store={store}>
-      <RecommendationPage />
+      <PersistGate loading={null} persistor={persitedStore} />
+      <RouterProvider router={router} />
     </ReduxProvider>
-  )
+  );
 }
-
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { persistStore } from 'redux-persist'
 
-import rootReducer from './rootReducer'
+import { persistedReducer } from './rootReducer'
 
 export const store = configureStore({
-  reducer: rootReducer
+  reducer: persistedReducer,
+  middleware: (defaultMiddleware) => defaultMiddleware({
+    serializableCheck: false
+  })
 })
+
+export const persitedStore = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
