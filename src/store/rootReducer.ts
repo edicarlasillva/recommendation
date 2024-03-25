3be9b88df9c56a0bf3d99b5f13e712933e9b6852
Carla Silva <carla.silva@movestock.com>
2024-03-25 20:54:46 -0300

    feat: added userSlice

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,11 +1,21 @@
 import { combineReducers } from '@reduxjs/toolkit'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 import recommendationsReducer from './slices/recommendationsSlice'
 import todoReducer from './slices/todoSlice'
+import userReducer from './slices/userSlice'
 
 const rootReducer = combineReducers({
   recommendations: recommendationsReducer,
-  todo: todoReducer
+  todo: todoReducer,
+  user: userReducer
 })
 
-export default rootReducer
+export const persistedReducer = persistReducer({
+  // chave no localStorage
+  key: 'alunos',
+  storage,
+}, rootReducer)
+
+// export default rootReducer
